718e45fa1dbc4da2af92d0a7dfc30803372a762c
Silver <yuyang.lin@dena.jp>
2022-08-11 23:26:03 +0800

    [UPDATE] show info

--- Assets/LoadData.cs
+++ Assets/LoadData.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-
-
+using System.IO;
 
 public class LoadData : MonoBehaviour
 {
     [SerializeField]
-    Button LoadObjBtn;
-
+    TMPro.TextMeshProUGUI info;
     [SerializeField]
     string AddressNameStr;
 
@@ -20,22 +18,44 @@ public class LoadData : MonoBehaviour
 
     void Start()
     {
-        LoadObjBtn.interactable = false;
         isLoadSucc = false;
-        Addressables.LoadAssetAsync<GameObject>(AddressNameStr).Completed += OnAssetObjLoaded;
-        LoadObjBtn.onClick.AddListener(CreateObjBtn);
     }
 
+    public void LoadAssetAsync()
+    {
+        info.text = "[LoadAssetAsync] loading...";
+        Addressables.LoadAssetAsync<GameObject>(AddressNameStr).Completed += OnAssetObjLoaded;
+    }
     void OnAssetObjLoaded(AsyncOperationHandle<GameObject> asyncOperationHandle)
     {
-        LoadObjBtn.interactable = true;
+        info.text = "[LoadAssetAsync] finish!";
         isLoadSucc = true;
         assetObj = asyncOperationHandle.Result;
     }
 
-    void CreateObjBtn()
+    public void CreateObjBtn()
     {
+        if (!isLoadSucc)
+        {
+            info.text = "[CreateObjBtn] LOAD FAILD!";
+            return;
+        }
+        info.text = "[CreateObjBtn]!";
         Vector3 pos = new Vector3(Random.Range(5, -5), Random.Range(4, -4), 0);
         Instantiate(assetObj, pos, Quaternion.identity);
+    }
+
+    public void ClearAllCache()
+    {
+        info.text = "[ClearAllCache] clear cache at " + Application.persistentDataPath;
+        var list = Directory.GetDirectories(Application.persistentDataPath);
+
+        foreach (var item in list)
+        {
+            info.text = info.text + "\nDelete: " + item;
+            Directory.Delete(item, true);
+        }
+
+        Caching.ClearCache();
     }
 }
