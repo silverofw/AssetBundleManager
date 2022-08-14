3408f2c06a79a48ab3d2d5a0ff64287d70e8fec8
Silver <yuyang.lin@dena.jp>
2022-08-14 17:52:18 +0800

    [UPDATE] AssetTool

---
+++ Assets/Editor/cardooo.utility/AssetTool.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetTool
+{
+    [MenuItem("GameObject/Cardooo/SelectionGameObjectPath", priority = 12)]
+    private static void SelectionGameObjectPath()
+    {
+        if (Selection.activeGameObject)
+        {
+            string text = GetTransPath(Selection.activeGameObject.transform);
+            GUIUtility.systemCopyBuffer = text;
+            Debug.Log(text);
+        }
+    }
+
+    /// <summary>
+    /// 获得GameObject在Hierarchy中的完整路径
+    /// </summary>
+    public static string GetTransPath(Transform trans)
+    {
+        if (!trans.parent)
+        {
+            return trans.name;
+        }
+        return $"{GetTransPath(trans.parent)}/{trans.name}";
+    }
+
+    /// <summary>
+    /// プロジェクトビューで1つ以上選択した対象Assetについて、Assetのパスを連結してクリップボードにセットする。
+    /// </summary>
+    [MenuItem("Assets/Cardooo/Copy Asset Path", false, 500)]
+    private static void CopyAssetPath()
+    {
+        List<string> lines = new List<string>();
+        foreach (var obj in Selection.objects)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+            lines.Add(path);
+        }
+        string text = string.Join("\n", lines);
+        GUIUtility.systemCopyBuffer = text;
+        Debug.Log(text);
+    }
+
+    /// <summary>
+    /// プロジェクトビューで1つ以上選択した対象Assetについて、AssetのGUIDを連結してクリップボードにセットする。
+    /// </summary>
+    [MenuItem("Assets/Cardooo/Copy Asset GUID", false, 500)]
+    private static void CopyAssetGuid()
+    {
+        List<string> lines = new List<string>();
+        foreach (var obj in Selection.objects)
+        {
+            string guid;
+            long localId;
+            AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj, out guid, out localId);
+            guid = guid ?? "";
+            lines.Add(guid);
+        }
+        string text = string.Join("\n", lines);
+        GUIUtility.systemCopyBuffer = text;
+        Debug.Log(text);
+    }
+}
